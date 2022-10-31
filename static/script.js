449295a3ba497db586ddca506b06a432b4945f6b
yzho285 <yzho285@uwo.ca>
2022-10-30 20:58:37 -0400

    search artist detail by artist id from front-end UI

--- static/script.js
+++ static/script.js
@@ -1,14 +1,27 @@
-const url = 'http://localhost:3000/api/genres';
+function SearchByartistID(){
+    let artistID = document.getElementById("searchartdetailbyartid").value;
+    const url = `http://ec2-44-202-137-17.compute-1.amazonaws.com:3000/api/raw_artists/${artistID}`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let result = data;
+        console.log(result);
+        let text = 'ArtistID:'+result.artist_id+'artist_active_year_begin:'+result.artist_active_year_begin+'artist_handle:'+result.artist_handle
+        +'artist_longitude:'+result.artist_longitude+'artist_members:'+result.artist_members+'artist_name:'+result.artist_name+'artist_related_projects:'
+        +result.artist_related_projects;
+        const getPos = document.getElementById("searchartdetailbytrackid");
+        const addDiv = document.createElement('div');
+        addDiv.id = "Div1";
+        textnode = document.createTextNode(text);
+        addDiv.appendChild(textnode);
+        document.body.insertBefore(addDiv,getPos);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    
+}
 
-fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    let genres = data;
-    console.log(genres);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
 
